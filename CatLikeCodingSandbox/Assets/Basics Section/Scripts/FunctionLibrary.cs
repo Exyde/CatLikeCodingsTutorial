1ef6ce0a33a08cecdb848808f0314a4c7bc42914
Exyde <54008677+Exyde@users.noreply.github.com>
2021-05-02 10:49:03 +0200

    Added Torus Function

--- CatLikeCodingSandbox/Assets/Basics Section/Scripts/FunctionLibrary.cs
+++ CatLikeCodingSandbox/Assets/Basics Section/Scripts/FunctionLibrary.cs
@@ -6,11 +6,11 @@ public static class FunctionLibrary
 {
 	public delegate Vector3 Function(float u, float v, float t);
 
-	public enum FunctionName { Wave, MultiWave, Ripple, Sphere};
+	public enum FunctionName { Wave, MultiWave, Ripple, Sphere, Torus};
 
 	static Function[] functions =
 	{
-		Wave, MultiWave, Ripple, Sphere
+		Wave, MultiWave, Ripple, Sphere, Torus
 	};
 
 	public static Function GetFunction (FunctionName name)
@@ -60,5 +60,16 @@ public static class FunctionLibrary
 		return p;
 	}
 
+	public static Vector3 Torus (float u, float v, float t)
+	{
+		float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+		float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+		float s = r1 + r2 * Cos(.5f * PI * v);
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		p.y = r2 * Sin(PI * v);
+		p.z = s * Cos(PI * u);
+		return p;
+	}
 
 }
